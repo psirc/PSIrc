7a977c68d6666aab3cb98bed9fa15f59d98d707b
Wojciech Sarwinski <01133073@pw.edu.pl>
2025-01-09 21:03:49 +0100

    added empty handle command methods

--- src/psirc/server.py
+++ src/psirc/server.py
@@ -1,26 +1,33 @@
-from connection_manager import ConnectionManager
+import socket
 from concurrent.futures import ThreadPoolExecutor
+from connection_manager import ConnectionManager
 from message_parser import MessageParser
+from message import Message
 from defines.commands import IRCCommand
 from identity_manager import IdentityManager
-from identity import IdentityType
+from identity import IdentityType, Identity
 from session_manager import SessionManager
 
 import logging
 
 
 class IRCServer:
-    def __init__(self, nick: str, host: str, port: int, max_workers: int = 10) -> None:
+    def __init__(
+            self,
+            nick: str, host: str, port: int,
+            max_workers: int = 10
+    ) -> None:
         self.running = False
         self.nick = nick
         self._thread_executor = ThreadPoolExecutor(max_workers)
         self._connection = ConnectionManager(host, port, self._thread_executor)
-        self._sessions = SessionManager()
+        self._sockets = SessionManager()
         self._identities = IdentityManager()
 
     def start(self) -> None:
         self.running = True
         self._connection.start()
+
         while self.running:
             client_socket, data = self._connection.get_message()
             message = MessageParser.parse_message(data)
@@ -46,10 +53,11 @@ class IRCServer:
                 # client has not set NICK and (USER or SERVER)
                 if message.command is IRCCommand.NICK:
                     # TODO : check for nick collisions
-                    identity.nick = message.params
+                    identity.nick = message.params.params
                 elif message.command is IRCCommand.USER and identity.nick:
                     # TODO : parse nick command
                     identity.type = IdentityType.USER
+                    self._sockets.add_user(identity.nick, client_socket)
                 # TODO : add for elif irccommand.server similar to command user
                 else:
                     logging.warning("Client was not registered")
@@ -64,3 +72,19 @@ class IRCServer:
             else:
                 # TODO : respond with unknown command error
                 pass
+
+    def handle_privmsg_command(
+            self,
+            client_socket: socket.socket,
+            identity: Identity,
+            message: Message
+    ) -> None:
+        pass
+
+    def handle_join_command(
+            self,
+            client_socket: socket.socket,
+            identity: Identity,
+            message: Message
+    ) -> None:
+        pass
