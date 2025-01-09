7156be500bbee9db11a2ce345100dcae4c7cdbeb
Wojciech Sarwinski <01133073@pw.edu.pl>
2025-01-09 20:04:38 +0100

    added some server utility

--- src/psirc/server.py
+++ src/psirc/server.py
@@ -1,18 +1,35 @@
 from connection_manager import ConnectionManager
 from concurrent.futures import ThreadPoolExecutor
+from message import Message, Prefix
 from message_parser import MessageParser
+from identity_manager import IdentityManager
+from defines.commands import IRCCommand
+from defines.responses import Command
+
+import logging
 
 
 class IRCServer:
-    def __init__(self, host: str, port: int, max_workers: int = 10) -> None:
+    def __init__(self, nick: str, host: str, port: int, max_workers: int = 10) -> None:
         self.running = False
+        self.nick = nick
         self._thread_executor = ThreadPoolExecutor(max_workers)
         self._connection = ConnectionManager(host, port, self._thread_executor)
+        self._identities = IdentityManager()
 
     def start(self) -> None:
         self.running = True
         self._connection.start()
         while self.running:
-            socket, data = self._connection.get_message()
+            client_socket, data = self._connection.get_message()
             message = MessageParser.parse_message(data)
-            message.command
+            identity = self._identities.get_identity(client_socket)
+
+            if not identity:
+                # client not registered accept only PASS command
+                if message.command is not IRCCommand.PASS:
+                    logging.warning("Client was not registered")
+                    client_socket.sendall(Message(prefix=Prefix(self.nick), command=Command(451)))
+                    continue
+                self._identities.add(client_socket)
+                # 
