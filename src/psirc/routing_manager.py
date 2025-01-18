1f29dfcdd4faccb33fddc3efdd1b57655afaf271
Wojciech Sarwinski <01133073@pw.edu.pl>
2025-01-18 22:38:48 +0100

    added a respond with error method

--- src/psirc/routing_manager.py
+++ src/psirc/routing_manager.py
@@ -2,14 +2,25 @@ import socket
 import logging
 
 from psirc.message import Message
+from psirc.response_params import parametrize
 from psirc.user_manager import UserManager
 from psirc.channel import Channel
+from psirc.defines.responses import Command
 
 
 class RoutingManager:
     @staticmethod
     def respond_client(client_socket: socket.socket, response: Message) -> None:
         client_socket.send(str(response).encode())
+
+    @staticmethod
+    def respond_client_error(client_socket, error_type: Command) -> None:
+        message_error = Message(
+            prefix=None,
+            command=error_type,
+            params=parametrize(error_type),
+        )
+        RoutingManager.respond_client(client_socket, message_error)
 
     @staticmethod
     def send_to_user(receiver_nick: str, message: Message, user_manager: UserManager) -> None:
@@ -30,6 +41,7 @@ class RoutingManager:
         # for now only sends to local
         encoded_message = str(message).encode()
         external_users = []
+        logging.info(f"Forwarding private message: {message}")
         for nickname in channel.users:
             user = user_manager.get_user(nickname)
             if user.is_local():
