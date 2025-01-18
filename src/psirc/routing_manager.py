c4e552f60e2f8dca03b8b2d61af533dbb4e6d67f
Wojciech Sarwinski <01133073@pw.edu.pl>
2025-01-18 12:58:30 +0100

    renamed to refactored names

--- src/psirc/routing_manager.py
+++ src/psirc/routing_manager.py
@@ -2,7 +2,7 @@ import socket
 import logging
 
 from psirc.message import Message
-from psirc.session_manager import SessionManager
+from psirc.user_manager import UserManager
 from psirc.channel import Channel
 
 
@@ -12,18 +12,28 @@ class RoutingManager:
         client_socket.send(str(response).encode())
 
     @staticmethod
-    def send_to_user(receiver_nick: str, message: Message, session_manager: SessionManager) -> None:
+    def send_to_user(receiver_nick: str, message: Message, user_manager: UserManager) -> None:
         # for now only sends to local
-        receiver_socket = session_manager.get_user_socket(receiver_nick)
-        if not receiver_socket:
+        receiver = user_manager.get_user(receiver_nick)
+        if not receiver:
             raise KeyError("Receiver not found")
-        logging.info(f"Forwarding private message: {message}")
-        receiver_socket.send(str(message).encode())
+        if receiver.is_local():
+            logging.info(f"Forwarding private message: {message}")
+            receiver.get_route().send(str(message).encode())
+        else:
+            # TODO: forward to server
+            raise NotImplementedError("send to user external")
         return
 
     @staticmethod
-    def send_to_channel(channel: Channel, message: Message, session_manager: SessionManager) -> None:
+    def send_to_channel(channel: Channel, message: Message, user_manager: UserManager) -> None:
         # for now only sends to local
         encoded_message = str(message).encode()
+        external_users = []
         for nickname in channel.users:
-            session_manager.get_user_socket(nickname).send(encoded_message)
+            user = user_manager.get_user(nickname)
+            if user.is_local():
+                user.get_route().send(encoded_message)
+            else:
+                external_users.append(user)
+        # TODO: forward to servers
