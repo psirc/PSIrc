955b1254e20ad959d6508d64e9cb5f54e455deda
doaads <nowobart@gmail.com>
2025-01-24 16:28:51 +0100

    feat: addr and port arguments

--- src/main.py
+++ src/main.py
@@ -1,12 +1,31 @@
+import argparse
 import logging
 import os
 from psirc.server import IRCServer
 
-# Configure logging
-logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
-conf_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "psirc.conf")
+def main() -> None:
+    parser = argparse.ArgumentParser(prog="psirc", description="PSI IRC Server")
+    
+    parser.add_argument("-a", "--address", dest="server_addr", default="127.0.0.1")
+    parser.add_argument("-p", "--port", dest="port", default="6667")
+    parser.add_argument("-n", "--name", dest="name", default="PSIrc Server")
 
-s = IRCServer("CSSetti2", "127.0.0.1", 6668, config_file=conf_file)
+    args = parser.parse_args()
 
-s.start()
+    address = args.server_addr
+    port = args.port
+    name = args.name
+
+    # Configure logging
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+
+    conf_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "psirc.conf")
+
+    s = IRCServer(name, address, int(port), config_file=conf_file)
+
+    s.start()
+
+
+if __name__ == "__main__":
+    main()
